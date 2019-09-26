2522a3f592b0e21851a9ad6a163ad66201ad2de6
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-09-26 17:27:31 +0530

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,25 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
-func isValidUser(h http.Handler) http.HandlerFunc {
+func isValidUser(h http.Handler) http.Handler {
 	validToken := "souvikhaldar"
-	return func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rcvdToken := r.Header.Get("token")
 		fmt.Println("Received token:", rcvdToken)
 		if rcvdToken == validToken {
 			h.ServeHTTP(w, r)
 		}
-	}
+	})
 }
 
 func writeMessage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("-----Runing writeMessage------ ")
-	w.Write([]byte("Hello world!"))
+	granularity := r.URL.Query().Get("granularity")
+	date := r.URL.Query().Get("date")
+	fmt.Println(granularity, date)
+	w.Write([]byte(fmt.Sprintf("Hello world!")))
 }
-func main() {
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8192", nil)
+func main() {
+	router := chi.NewRouter()
+	router.Handle("/", http.FileServer(http.Dir(".")))
+	router.Get("/hello", writeMessage)
+	http.ListenAndServe(":8192", router)
 }
